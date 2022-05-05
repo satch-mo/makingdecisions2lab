63a144952d81fc36fa1a06b0d8efda4bfee37af3
satch-mo <satchel0293@gmail.com>
2022-05-05 15:58:53 -0500

    save before EOD

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -100,7 +100,7 @@ var myFavoriteNumbers = [4, 8, 12, 16, 20, 24];
 //Code Here
 let someNum = myFavoriteNumbers[4];
 
-console.log(someNum);
+// console.log(someNum);
 
 ////////// PROBLEM 7 //////////
 
@@ -108,6 +108,13 @@ console.log(someNum);
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
+if (myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array");
+} else if (myFavoriteNumbers.length >= 7) {
+  someNum = myFavoriteNumbers[6];
+}
+
+console.log(someNum);
 
 ////////// PROBLEM 8 //////////
 
